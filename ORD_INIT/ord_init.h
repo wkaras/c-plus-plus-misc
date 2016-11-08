d10caa4707261bfbbc801eb7c3ac228d1853f376
Walt Karas <wkaras@yahoo.com>
2016-11-07 19:35:05 -0500

    Initial commit.

---
+++ ORD_INIT/ord_init.h
@@ -0,0 +1,180 @@
+/*
+Copyright (c) 2016 Walter William Karas
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+The Ord_init class template helps to order the intialization of non-local
+objects in the same executable but in different compilation units.  Suppose
+
+A a;
+
+and
+
+B b;
+
+are in different compilation units, but b's constructor depends on object a
+being already initialized.  Then the declaration of b must be changed to
+
+Ord_init<B> b;
+
+The first use of b by class A's constructor must be a call to b.init().
+Class B must have a parameterless constructor.  The object b will implicitly
+convert to B &, and b() will return B &.
+
+If the constructor for class B depends on another object, c say, then c
+must be declared using Ord_init:
+
+Ord_init<C> c;
+
+even if c is defined in the same compilation unit as b.  B's constructor must
+call c.init() before making any other use of c.
+
+Ord_init takes a second class parameter called Traits, which defaults to
+Ort_init_default_traits.  If using a version of C++ earlier than C++11,
+Traits must define the public type Align_type.  Align_type must be a POD
+type, with the same alignment requirements as the first type parameter to
+Ord_init.  Traits must have a public member function cycle(uintptr_t).
+This is called when a cyclical initialization dependency of Ord_init objects
+is detected.
+
+This facility does not provide any guarantees as to the order that objects
+are destroyed in.
+
+For Ord_init to work properly, all the objects defined with it, and those
+that depend on them, must be constructed within the same execution thread.
+
+*/
+
+#ifndef ORD_INIT_20161107
+#define ORD_INIT_20161107
+
+#include <iostream>
+#include <exception>
+
+#if __cplusplus < 201100
+
+#include <stdint.h>
+
+#else
+
+#include <cstdint>
+
+#endif
+
+struct Ord_init_default_traits
+  {
+    #if __cplusplus < 201100
+    typedef int Align_type;
+    #endif
+
+    #if __cplusplus >= 201100
+    typedef std::uintptr_t uintptr_t;
+    #endif
+
+    class Ord_init_cycle_exception : public std::exception
+      {
+      public:
+
+        virtual const char * what() const
+        #if __cplusplus >= 201100
+        noexcept
+        #else
+        throw()
+        #endif
+        { return("Ord_init constructor encountered cycle"); }
+      };
+
+    static void cycle(uintptr_t addr)
+      {
+        std::cerr
+          << "Cyclical initializaion dependencies for object at address 0x"
+          << std::hex << addr << '\n';
+
+        throw Ord_init_cycle_exception();
+      }
+  };
+
+template <class T, class Traits = Ord_init_default_traits>
+class Ord_init
+  {
+  private:
+
+    enum Status { Pre_init = 0, Init_in_progress, Init_done };
+
+    // It's important that this member be initialized to zero
+    // before any constructors are called.
+    Status status;
+
+    #if __cplusplus < 201100
+
+    union
+      {
+        typename Traits::Align_type dummy;
+        char raw[sizeof(T)];
+      };
+
+    #else
+
+    alignas(T) char raw[sizeof(T)];
+
+    typedef std::uintptr_t uintptr_t;
+
+    #endif
+
+  public:
+
+    void init()
+      {
+        if (status == Init_done)
+          return;
+
+        if (status == Init_in_progress)
+          Traits().cycle(reinterpret_cast<uintptr_t>(this));
+
+        status = Init_in_progress;
+
+        new(raw) T;
+
+        status = Init_done;
+      }
+
+    Ord_init() { init(); }
+
+    T & operator () () { return(* reinterpret_cast<T *>(raw)); }
+
+    operator T & () { return((*this)()); }
+
+    ~Ord_init() { reinterpret_cast<T *>(raw)->~T(); }
+
+  // No copying except through T reference.
+  #if __cplusplus < 201100
+
+  private:
+    Ord_init(const Ord_init &);
+    Ord_init & operator = (const Ord_init &);
+
+  #else
+
+    Ord_init(const Ord_init &) = delete;
+    Ord_init & operator = (const Ord_init &) = delete;
+
+  #endif
+
+  }; // end Ord_init
+
+#endif // Include once.
---
+++ ORD_INIT/test.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+#include "ord_init.h"
+
+template <char C>
+struct X
+  {
+    X() { std::cout << "construct " << C << '\n'; }
+    ~X() { std::cout << "destroy " << C << '\n'; }
+  };
+
+template <char C>
+class Dep
+  {
+  private:
+    void cons();
+
+  public:
+    Dep() { cons(); }
+  };
+
+template <char C>
+struct Y : private Dep<C>, public X<C> { };
+
+template <char C>
+struct Z : public Ord_init< Y<C> > { };
+
+extern Z<'a'> a;
+extern Z<'b'> b;
+extern Z<'c'> c;
+extern Z<'d'> d;
+extern Z<'e'> e;
+extern Z<'f'> f;
+extern Z<'g'> g;
+
+#if CU == 1
+
+Z<'a'> a;
+
+template <>
+void Dep<'a'>::cons() { e.init(); c.init(); }
+
+#elif CU == 2
+
+Z<'b'> b;
+
+template <>
+void Dep<'b'>::cons() { g.init(); }
+
+Z<'c'> c;
+
+template <>
+void Dep<'c'>::cons() { b.init(); }
+
+Z<'d'> d;
+
+template <>
+void Dep<'d'>::cons() { f.init(); }
+
+Z<'e'> e;
+
+template <>
+void Dep<'e'>::cons() { d.init(); }
+
+#elif CU == 3
+
+Z<'f'> f;
+
+#ifdef FAIL
+
+template <>
+void Dep<'f'>::cons() { a.init(); }
+
+#else
+
+template <>
+void Dep<'f'>::cons() { }
+
+#endif
+
+Z<'g'> g;
+
+template <>
+void Dep<'g'>::cons() { f.init(); }
+
+int main() { return(0); }
+
+#endif
